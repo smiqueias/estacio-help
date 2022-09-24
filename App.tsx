09f27d67008e2714e8d386a2e9793e763268881d
Saulo <saulomiqueias7@gmail.com>
2022-09-23 23:15:26 -0300

    feat: add color scheme

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import { Box, NativeBaseProvider, StatusBar } from "native-base";
 import React from "react";
+import { THEME } from "./src/styles/theme";
 
 export default function App() {
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle={"light-content"}
         backgroundColor={"transparent"}
