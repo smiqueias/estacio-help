430c815781dc7065ea41d11a1644bf413fd58ad6
higor.francisco <higor.francisco@acpgroup.com.br>
2022-09-28 22:09:35 -0300

    Registro

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,10 +1,53 @@
-import { Box, Text } from "native-base";
-import React from "react";
+import { Box, VStack } from "native-base";
+import React, { useState } from "react";
+import { Button } from "../components/Button";
+import { Alert } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+import { Header } from "../components/Header";
+import { Input } from "../components/Input";
 
 export function Register() {
+  const navigation = useNavigation();
+  const [isLoading, setIsLoading] = useState(false);
+  const [patrimony, setPatrimony] = useState("");
+  const [description, setDescription] = useState("");
+  async function handleNewOrderRegister() {
+    if (!patrimony || !description) {
+      return Alert.alert("Cadastrar", "Informe o patrimônio e a descrição.");
+    }
+    return (
+      console.log(patrimony),
+      Alert.alert("Solicitação", "Solicitação registrada com sucesso"),
+      navigation.goBack()
+    );
+  }
   return (
-    <Box>
-      <Text>Register</Text>
-    </Box>
+    <VStack flex={1} p={6} bg="gray.600">
+    
+      <Header title="Solicitação" />
+      <Input
+        placeholder="Numéro do Património"
+        mt={2}
+        onChangeText={setPatrimony}
+      />
+      <Input
+        placeholder="Descrição do problema"
+        flex={1}
+        mt={6}
+        multiline
+        textAlignVertical="top"
+        onChangeText={setDescription}
+        minH={24}
+      />
+      <Box p={4}>
+        <Button
+          title="Cadastrar"
+          mt={5}
+          onPress={handleNewOrderRegister}
+          isLoading={isLoading}
+        />
+      </Box>
+    </VStack>
   );
 }
