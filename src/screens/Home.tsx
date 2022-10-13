267c42ec859fbade9fb80ab24676326016e2f8a8
higor.francisco <higor.francisco@acpgroup.com.br>
2022-10-13 18:31:26 -0300

    versão gessica, atualização higor

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,48 @@
-import { Box, Text } from "native-base";
-import React from "react";
 
+import { Box, Center, Text } from "native-base";
+import React from "react";
+import { VStack } from "native-base";
+import { Button } from "../components/Button";
+import { useNavigation } from "@react-navigation/native";
+import { Header } from "../components/Header";
 export function Home() {
+  const navigation = useNavigation();
+  function navigateToHomeScreen() {
+    navigation.navigate("home");
+  }
+
+  function navigateToRegisterScreen() {
+    navigation.navigate("register");
+  }
+  function navigateToDetailsScreen() {
+    navigation.navigate("details");
+  }
   return (
-    <Box>
-      <Text>HOME</Text>
-    </Box>
+    <VStack flex={1} pb={6} bg="gray.700">
+      <Header title="Solicitação" />
+      <VStack space={4} alignItems="center" mt={48}>
+        <Button
+          width={300}
+          title="Home"
+          isLoading={false}
+          onPress={navigateToHomeScreen}
+          mt={5}
+        />
+        <Button
+          width={300}
+          title="Register"
+          isLoading={false}
+          onPress={navigateToRegisterScreen}
+          mt={5}
+        />
+        <Button
+          width={300}
+          title="Details"
+          isLoading={false}
+          onPress={navigateToDetailsScreen}
+          mt={5}
+        />
+      </VStack>
+    </VStack>
   );
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,12 +1,47 @@
-import * as React from "react";
-import { Box, Image, Text, Heading, VStack, FormControl, Input, Link, Button, HStack, Center, NativeBaseProvider, Icon } from "native-base";
-import MaterialIcons from "@expo/vector-icons/MaterialCommunityIcons";
-export function SignIn() {
+import React, { useState } from "react";
+import { Alert } from "react-native";
 
+import { Box, Image, Text, Heading, VStack, FormControl, Input, Link, Button, HStack, Center, NativeBaseProvider, Icon, useTheme } from "native-base";
+import MaterialIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { useNavigation } from "@react-navigation/native";
+
+export function SignIn() {
+  const { colors } = useTheme();
+  const navigation = useNavigation();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [isLoading, setIsLoading] = useState(false);
+  function handleSignIn() {
+    const i = "admin"
+    const s = '12345'
+    if (!email || !password) {
+      setIsLoading(false);
+      return Alert.alert("Entrar", "Informe e-mail e senha")
+    }
+
+    if(email == i && password == s){
+           return navigation.navigate("home");
+ 
+    }
+      return Alert.alert("Insira o email e senha");
+
+    // return (
+    //   Alert.alert("Solicitação", "Solicitação registrada com sucesso"),
+    //   navigation.goBack()
+    // )
+
+  }
   return (
     <Center w="100%">
       <Box safeArea p="2" py="2" w="90%" mt="20%" maxW="290">
-      <VStack alignItems="center">
+        <VStack alignItems="center">
+          {/* <Image
+            mt="12"
+            source={require('../../assets/estacio.png')}
+            style={{ width: 80, height: 80 }}
+          /> */}
           <Heading mt="1" _dark={{
             color: "warmGray.200"
           }} color="coolGray.600" fontWeight="high" size="md">
@@ -24,11 +59,11 @@ export function SignIn() {
         <VStack space={1} mt="4">
           <FormControl>
             <FormControl.Label></FormControl.Label>
-            <Input type="" InputLeftElement={<Icon as={<MaterialIcons name="email" />} size={5} ml="2" color="muted.400" />} placeholder="Email" />
+            <Input type="" onChangeText={setEmail} InputLeftElement={<Icon as={<MaterialIcons name="email" />} size={5} ml="2" color="muted.400" />} placeholder="Email" />
           </FormControl>
           <FormControl>
             <FormControl.Label></FormControl.Label>
-            <Input type="password" InputLeftElement={<Icon as={<MaterialIcons name="key-variant" />} size={5} ml="2" color="muted.400" />} placeholder="Senha" />
+            <Input type="password" onChangeText={setPassword} InputLeftElement={<Icon as={<MaterialIcons name="key-variant" />} size={5} ml="2" color="muted.400" />} placeholder="Senha" />
             <Link _text={{
               fontSize: "xs",
               fontWeight: "500",
@@ -37,7 +72,7 @@ export function SignIn() {
               Esqueceu a senha?
             </Link>
           </FormControl>
-          <Button mt="2" color="primary.400">
+          <Button onPress={handleSignIn} mt="2" color="primary.400">
             Entrar
           </Button>
           <HStack mt="6" justifyContent="center">
